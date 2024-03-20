c31411b8d802122a01ea767e1838e6d13f7a0b84
rahul <shyamrahul815@gmail.com>
2024-03-20 15:22:03 +0530

    third commit

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'task-management-4m8s.onrender.com',
+    baseUrl: 'https://task-management-api-ybrt.onrender.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
